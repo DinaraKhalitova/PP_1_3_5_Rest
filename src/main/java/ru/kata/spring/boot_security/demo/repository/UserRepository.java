de0b36c013588cb7caa06b2a4bab227c43e9fe05
”Dinara <d.xalitowa2916@gmail.com>
2024-06-21 14:54:52 +0500

    3-1-4-update2-1. Растянула таблицу по ширине экрана 2. Убрала вкладку админ со страницы юзера 3. Поле с возрастом сделала числовым 4. Удалила лишний класс из папки "security" 5. Убрала @Column(name = "id") 6. Поправила класс Role

--- src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/UserRepository.java
@@ -6,6 +6,7 @@ import ru.kata.spring.boot_security.demo.model.User;
 
 import java.util.Optional;
 
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String userName);
